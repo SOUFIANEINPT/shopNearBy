78c85b22cb1ea1159fb1c316fa499d3bbdab91ad
soufiane <soufiane.elmoudden.15@gmail.com>
2018-03-30 13:59:18 +0100

    frontend

---
+++ angular_frontend/shops/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { MyPreferredShopsComponent } from './my-preferred-shops/my-preferred-shops.component';
+import { NearbyShopsComponent } from './nearby-shops/nearby-shops.component';
+import { AuthService } from './auth/auth.service';
+import { AuthGuard} from './auth/auth-guard.service';
+
+const appRoutes: Routes=[
+  { path: 'login', component: LoginComponent},
+  { path: 'register',component: RegisterComponent},
+  { path: 'MyPreferredShops', component:MyPreferredShopsComponent, canActivate:[AuthGuard]},
+  { path: 'NearbyShops',component:NearbyShopsComponent,canActivate:[AuthGuard]},
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyPreferredShopsComponent,
+    NearbyShopsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [AuthService,AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
